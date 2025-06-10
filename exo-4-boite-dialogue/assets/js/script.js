070d18ad4bbd7184cfea2e9a7ca2ea7672f4be7f
daniel <danielaraux@hotmail.com>
2025-06-10 16:27:27 +0200

    exo-5-fini

--- exo-4-boite-dialogue/assets/js/script.js
+++ exo-4-boite-dialogue/assets/js/script.js
@@ -1,4 +1,12 @@
-// Commande pour faire afficher une pop-up sur la page qui va afficher Hello World (pas obligé de mettre de ; si il n'y a qu'une instruction par ligne)
-alert('Test Javascript');
+let nom = prompt("Quel est votre nom ?")
+let prenom = prompt("Quel est votre prenom ?")
+let age = prompt("Quel est votre age ?")
+let email = prompt("Quel est votre email ?")
 
-console.log(85);
+// Pour créer des prompts avec champ à remplir qui vont définir le contenu des variables
+
+
+
+alert("Nom : " + nom + "\nPrenom : " + prenom + "\nAge : " + age + " ans" + "\nemail : " + email)
+
+// Pour afficher une pop-up a la fin qui affichera le résultat des saisies (le \n permet le retour à la ligne)
---
+++ exo-6-boite-quel-est-ton-type/assets/js/script.js
@@ -0,0 +1,9 @@
+let secret = "mot de passe"
+
+console.log("La variable secret est de type " + (typeof secret))
+
+// on change la valeur
+
+
+secret = 777
+
