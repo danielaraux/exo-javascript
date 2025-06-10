d6e55cc1649e9aa3fbbb622e5d5176bf378b01eb
daniel <danielaraux@hotmail.com>
2025-06-10 13:52:29 +0200

    exo-2-fini

--- exo-2-variable-partone-parttwo/assets/js/script.js
+++ exo-2-variable-partone-parttwo/assets/js/script.js
@@ -1,4 +1,6 @@
-// Commande pour faire afficher une pop-up sur la page qui va afficher Hello World (pas obligé de mettre de ; si il n'y a qu'une instruction par ligne)
-alert('Test Javascript');
+let partOne = 'Hello'
+let partTwo = 'World'
 
-console.log(85);
+alert(partOne + " " + partTwo)
+
+// On déclare nos deux variables, on utilise alert pour créer une pop-up et on les "concatène" dans l'alert pour qu'ils soient affichés sur la même pop-up
